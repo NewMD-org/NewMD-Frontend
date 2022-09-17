3a614981fd31f8674bc2e1425abeb1022a019191
Alex <anonymousaaaa41414141@gmail.com>
2022-09-17 11:55:58 +0800

    Test

--- src/pages/Table/index.js
+++ src/pages/Table/index.js
@@ -1,13 +1,13 @@
-import { useRef, useState, useEffect } from 'react';
+// import { useRef, useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import cookie from 'react-cookies';
-import axios from '../../api/MdTimetableAPI';
+// import axios from '../../api/MdTimetableAPI';
 import NavbarTop from './components/navbarTop';
 import jwt_decode from 'jwt-decode';
 import './index.css';
 
 
-const loginAPI = '/cloud/users/login';
+// const loginAPI = '/cloud/users/login';
 // document.body.onload = addElement("d1n1", "班會", "呂信寬");
 
 // function addElement(id, classname, teacher) {
@@ -23,7 +23,7 @@ const loginAPI = '/cloud/users/login';
 
 
 const Table = () => {
-    const [isLoading, setLoading] = useState(false);
+    // const [isLoading, setLoading] = useState(false);
 
     // const [d1n1_C, set_d1n1_C] = useState('');
     // set_d1n1_C('123');
